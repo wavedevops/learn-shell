c9b2ae370e54e992cbf77946039e5981b49db475
Chowdary-Hari <chandrahari321@outlook.com>
2025-07-29 18:44:54 +0530

    Automated commit #1363

--- Robho-shop/common.sh
+++ Robho-shop/common.sh
@@ -1,6 +1,6 @@
 code_check() {
   if [ $? -eq 0 ]; then
-    echo -e "\e[35msuccess\e[0m"
+    echo -e "\e[32msuccess\e[0m"
   else
     echo -e "\e[31mfailure\e[0m"
     exit 1
@@ -8,5 +8,5 @@ code_check() {
 }
 
 print_head() {
-  echo -e "\e[32m>>>>>>> $1 <<<<<<<\e[0m"
+  echo -e "\e[33m>>>>>>> $1 <<<<<<<\e[0m"
 }
