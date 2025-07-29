d4a66866274f8f69940047c3a1bb00a899851fa4
Chowdary-Hari <chandrahari321@outlook.com>
2025-07-29 18:47:47 +0530

    Automated commit #1364

--- Robho-shop/frontend.sh
+++ Robho-shop/frontend.sh
@@ -1,35 +1,35 @@
 #!/bin/bash
 source common.sh
-
-print_head
-code_check
+log_file=/tmp/expense.log
+#print_head
+#code_check
 
 print_head "module disable nginx"
-dnf module disable nginx -y
+dnf module disable nginx -y &>>log_file
 code_check
 
 print_head "module enable nginx"
-dnf module enable nginx:1.24 -y
+dnf module enable nginx:1.24 -y &>>log_file
 
 print_head "install nginx"
 dnf install nginx -y
 code_check
 
 print_head "remove the default HTML content"
-rm -rf /usr/share/nginx/html/*
+rm -rf /usr/share/nginx/html/* &>>log_file
 code_check
 
 print_head "download frontend content"
-curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend-v3.zip
+curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend-v3.zip &>>log_file
 code_check
 
 print_head "unzip frontend content"
-cd /usr/share/nginx/html
+cd /usr/share/nginx/html &>>log_file
 unzip /tmp/frontend.zip
 code_check
 
 print_head "add the config"
-cp nginx.conf /etc/nginx/nginx.conf
+cp nginx.conf /etc/nginx/nginx.conf &>>log_file
 code_check
 
 print_head "Start & Enable Nginx service"
