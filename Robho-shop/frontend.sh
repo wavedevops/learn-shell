6480690256931a2525748db9f1dff996bd87a565
Chowdary-Hari <chandrahari321@outlook.com>
2025-07-29 18:55:02 +0530

    Automated commit #1367

--- Robho-shop/frontend.sh
+++ Robho-shop/frontend.sh
@@ -12,13 +12,14 @@ print_head "install nginx"
 dnf install nginx -y &>>log_file
 code_check
 
-print_head "Start & Enable Nginx service"
-systemctl enable nginx
-systemctl restart nginx
-code_check
+
 
 print_head "remove the default HTML content"
 rm -rf /usr/share/nginx/html/* &>>log_file
+code_check
+
+print_head "add the config"
+cp nginx.conf /etc/nginx/nginx.conf &>>log_file
 code_check
 
 print_head "download frontend content"
@@ -30,9 +31,6 @@ cd /usr/share/nginx/html &>>log_file
 unzip /tmp/frontend.zip &>>log_file
 code_check
 
-print_head "add the config"
-cp nginx.conf /etc/nginx/nginx.conf &>>log_file
-code_check
 
 print_head "Start & Enable Nginx service"
 systemctl enable nginx
